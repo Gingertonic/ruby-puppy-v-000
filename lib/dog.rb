d75c907c8d3c5a052d23977bcfeefbf20fcbeb6c
Beth Schofield <bethmschofield@gmail.com>
2018-03-07 04:54:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,14 +2,14 @@ class Dog
   @@all = []
 
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
 
   def self.all
-    puts @all
+    puts @@all
   end
-    
+
 end
